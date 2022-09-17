c1c064cee8355c137ff64fb79b05808ab8e01c9d
ArthurFutataaa <arthur302508@gmail.com>
2022-09-17 00:17:30 -0300

    commit att localidades

--- js/localidades.js
+++ js/localidades.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
     $('#cidade').hide();
@@ -6,10 +6,15 @@ $(document).ready(function(){
     $.ajax({
         type: 'GET',
         dataType: 'json',
-        data: {orderBy: "nome"},
+        data: { orderBy: "nome" }, // OrderBy: Deixar nomes em ordem
         url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados`,
-        success:function(dados) {
-            
+        success: function(response) {
+            $.each(response, function(indexInArray, valueOfElement) {
+                var option = "<option>" + valueOfElement.sigla + "<option>" // criando uma variável contendo a tag HTML
+                $("#uf").append(option) // Colocando a variável de HTML dentro da id uf
+            })
         }
-    });
-})
+    })
+});
+
+// Mudando a opção uf
